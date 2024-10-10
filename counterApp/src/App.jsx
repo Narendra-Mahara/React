71bb08e049803e9c1360e673e6b906441ad70fa4
Narendra Mahara <narendramahara85131@gmail.com>
2024-10-10 16:27:07 +0545

    learn useState hook

--- counterApp/src/App.jsx
+++ counterApp/src/App.jsx
@@ -1,11 +1,38 @@
-import './App.css'
-
+import "./App.css";
+import { useState } from "react";
 function App() {
+  let [count, setCount] = useState(0);
+
+  let increaseValue = () => {
+    // if (count === 10) {
+    //   alert("You have reached the maximum limit");   // stop the counter at 10
+    //   return;
+    // }
+    setCount(count + 1);
+  };
+
+  let decreaseValue = () => {
+    // if (count == 0) {
+    //   alert("You have reached the minimum limit");  // stop the counter at 0
+    //   return;
+    // }
+    setCount(count - 1);
+  };
   return (
     <>
-     <h1>Counter App</h1>
+      <h1>Counter App</h1>
+
+      <div className="container">
+        <h3>Count : {count}</h3>
+        <button type="button" onClick={increaseValue}>
+          increase value
+        </button>
+        <button type="button" onClick={decreaseValue}>
+          decrease value
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
